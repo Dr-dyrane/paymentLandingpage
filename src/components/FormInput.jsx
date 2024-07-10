94766744b3b279b278d6dc923c72c74a007f6683
Dr-dyrane <halodyrane@gmail.com>
2024-07-10 07:39:54 +0100

    fix input

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const FormInput = ({ label, type, value, onChange, error }) => (
-  <div className="mb-4">
-    <label className="block text-contrast font-semibold mb-2">{label}</label>
-    <input
-      type={type}
-      value={value}
-      onChange={onChange}
-      className={`w-full p-2 border rounded-lg ${error ? 'border-red-500' : 'border-gray-300'}`}
-      required
-    />
-    {error && <p className="text-red-500 text-sm">{error}</p>}
-  </div>
+const FormInput = ({
+	label,
+	type,
+	value,
+	onChange,
+	error,
+	name,
+	placeholder,
+}) => (
+	<div className="mb-4">
+		<label className="block text-contrast font-semibold mb-2">{label}</label>
+		<input
+			type={type}
+			value={value}
+			onChange={onChange}
+			name={name}
+			placeholder={placeholder}
+			className={`w-full p-2 border rounded-lg ${
+				error ? "border-red-500" : "border-gray-300"
+			}`}
+			required
+		/>
+		{error && <p className="text-red-500 text-sm">{error}</p>}
+	</div>
 );
 
 export default FormInput;
