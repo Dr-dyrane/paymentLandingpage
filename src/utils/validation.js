085c3a56b08734411f610a293b1568adf576808f
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2024-07-15 15:37:37 +0100

    Update validation.js

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,10 +1,33 @@
+// utils/validation.js
+
 export const validate = (values) => {
-    let errors = {};
-    if (!values.email) errors.email = "Email is required";
-    if (!values.amount) errors.amount = "Amount is required";
-    if (!values.cardNumber) errors.cardNumber = "Card Number is required";
-    if (!values.expiryDate) errors.expiryDate = "Expiry Date is required";
-    if (!values.cvc) errors.cvc = "CVC is required";
-    return errors;
-  };
-  
+  let errors = {};
+
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = "Email address is invalid";
+  }
+
+  if (!values.amount) {
+    errors.amount = "Amount is required";
+  } else if (isNaN(values.amount)) {
+    errors.amount = "Amount must be a number";
+  }
+
+  if (!values.firstName) {
+    errors.firstName = "First Name is required";
+  }
+
+  if (!values.lastName) {
+    errors.lastName = "Last Name is required";
+  }
+
+  if (!values.phone) {
+    errors.phone = "Phone number is required";
+  } else if (!/^\d+$/.test(values.phone)) {
+    errors.phone = "Phone number must contain only digits";
+  }
+
+  return errors;
+};
