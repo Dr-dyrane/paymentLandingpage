76cea9f36d6b2f5e1ae4a9c1f3f99e53a2ee4595
Dr-dyrane <halodyrane@gmail.com>
2024-07-16 20:27:12 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [react()],
-	define: {
-		"process.env": {
-			VITE_SAY_SWITCH_PUBLIC_KEY: process.env.VITE_SAY_SWITCH_PUBLIC_KEY,
-		},
-	},
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App Name',
+        description: 'Description of your app',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#007BFF',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '64x64',
+            type: 'image/x-icon'
+          },
+          {
+            src: '/favicon.ico',
+            sizes: '512x512',
+            type: 'image/x-icon'
+          }
+        ]
+      }
+    })
+  ],
+  define: {
+    "process.env": {
+      VITE_SAY_SWITCH_PUBLIC_KEY: process.env.VITE_SAY_SWITCH_PUBLIC_KEY,
+    },
+  },
 });
